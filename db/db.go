d92073915f29b66979058c091a3b68c76822b577
Dka09228 <119800310+Dka09228@users.noreply.github.com>
2024-06-02 18:42:51 +0500

    db seperation

---
+++ db/db.go
@@ -0,0 +1,40 @@
+package db
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"time"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+const (
+	MongoURI       string = "mongodb://localhost:27017"
+	DatabaseName   string = "golangInterview"
+	CollectionName string = "posts"
+)
+
+// ConnectMongoDB creates and returns a connection to the MongoDB
+func ConnectMongoDB() (*mongo.Client, error) {
+	clientOptions := options.Client().ApplyURI(MongoURI)
+	client, err := mongo.Connect(context.TODO(), clientOptions)
+	if err != nil {
+		log.Fatalf("Failed to connect to MongoDB: %v", err)
+		return nil, err
+	}
+
+	// Check the connection
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	err = client.Ping(ctx, nil)
+	if err != nil {
+		log.Fatalf("Failed to ping MongoDB: %v", err)
+		return nil, err
+	}
+
+	fmt.Println("Connected to MongoDB!")
+	return client, nil
+}
